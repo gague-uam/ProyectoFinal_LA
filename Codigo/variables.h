6c3f5957f89b1178a15a69e1a681e17016adc37f
jevg7 <jvegag@uamv.edu.ni>
2024-06-27 23:39:24 -0600

    Version final

---
+++ Codigo/variables.h
@@ -0,0 +1,30 @@
+#define MAX 100
+#define MAXID 14
+
+ struct ESTUDENTS{
+    std::string NAME;
+    int CIF;
+    int AGE;
+    std::string CAREER ;
+}persona;
+
+ struct REGISTRERS{
+   std::string ID  ;
+   std:: string NAME  ; 
+}persona2;
+
+struct MATCHES{
+    int ID;
+    std::string DATE;
+    std::string HOUR;
+    std::string LOCAL;
+    std::string VISIT;
+    int LOCALPOINTS;
+    int VISITPOINTS;
+}partido[MAX];
+
+int contador = 0;
+
+
+
+
